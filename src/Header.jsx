a6ecde1e910feaa234f1336aedd2b9e2344d865f
Mukeshthakkur <mukoographics.mg@gmail.com>
2021-05-29 00:17:22 +0500

    Linkedin-clone app created

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="Header">
+            <h2>its Header</h2>
+
+            <div className="header__left">
+                <img src="" alt="" />
+
+                <div className="header__search">
+                    
+                </div>
+            </div>
+
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
